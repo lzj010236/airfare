4732b95fa02b70eac62e4fbe157c92d8aa950ead
J <lzj010236@gmail.com>
2015-05-09 19:15:26 -0400

    first commit

---
+++ airfare/airfare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  airfare
+//
+//  Created by Jing Lin on 5/9/15.
+//  Copyright (c) 2015 Jing Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ airfare/airfare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  airfare
+//
+//  Created by Jing Lin on 5/9/15.
+//  Copyright (c) 2015 Jing Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
