624629df00f69eba0e6eceacf02d32f887a6c907
J <lzj010236@gmail.com>
2015-05-15 21:30:19 -0400

    v1

---
+++ airfareWeb/index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="show_results.php" method="get">
+Origin: <input type="text" name="org" value="PIT"><br>
+Destination: <input type="text" name="dest" value="SFO"><br>
+Time: <input type="date" name="time" value="2015-10-01"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
